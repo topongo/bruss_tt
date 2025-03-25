7a8db16e07ab22bd1446e1d8b6c354f3c9953611
topongo <lorenzo.bodini.private@gmail.com>
2025-03-25 10:59:43 +0100

    feat: add client telemetry for statistics purposes

--- src/client.rs
+++ src/client.rs
@@ -8,16 +8,33 @@ use reqwest::{Client, RequestBuilder, Method};
 use serde::Serialize;
 use serde_json::error::Category;
 use log::{debug, info, trace};
+use tokio::sync::Mutex;
 
 pub struct TTClient {
     base_url: String,
     secret: String,
-    client: Client
+    client: Client,
+    telemetry: Mutex<Telemetry>,
+}
+
+#[derive(Debug)]
+struct Telemetry {
+    pub requests: usize,
+    // pub bytes_sent: u64,
+    pub bytes_received: usize,
+    // pub errors: u64,
+    pub instant: std::time::Instant,
+}
+
+impl Default for Telemetry {
+    fn default() -> Self {
+        Self { requests: 0, bytes_received: 0, instant: std::time::Instant::now() }
+    }
 }
 
 impl TTClient {
     pub fn new(base_url: String, secret: String) -> Self {
-        Self { base_url, secret, client: Client::new() }
+        Self { base_url, secret, client: Client::new(), telemetry: Mutex::new(Telemetry::default()) }
     }
 
     pub(super) fn auth_req(&self, method: Method, url: &str, extra: Option<String>) -> RequestBuilder {
@@ -37,11 +54,17 @@ impl TTClient {
         let r = self.build_request(options.unwrap_or(RequestOptions::new(O::ENDPOINT)), None);
         info!("generated request: {:?}", r);
         info!("requesting endpoint {}", O::ENDPOINT);
-        let body = r 
+        let response = r 
             .send()
-            .await?
+            .await?;
+
+        debug!("response: {:?}", response);
+        self.telemetry.lock().await.requests += 1;
+        let body = response
             .text()
             .await?;
+
+        self.telemetry.lock().await.bytes_received += body.as_bytes().len();
 
         trace!("received text from TT: {body}");
         serde_json::from_str::<Vec<O>>(&body).map_err(|e| {
@@ -65,11 +88,17 @@ impl TTClient {
         let r = self.build_request(options.unwrap_or(RequestOptions::new(O::ENDPOINT)), Some(id.clone()));
         info!("generated request: {:?}", r);
         info!("requesting endpoint {} and id {}", O::ENDPOINT, id);
-        let body = r 
+        let response = r 
             .send()
-            .await?
+            .await?;
+
+        debug!("response: {:?}", response);
+        self.telemetry.lock().await.requests += 1;
+        let body = response
             .text()
             .await?;
+        
+        self.telemetry.lock().await.bytes_received += body.as_bytes().len();
 
         trace!("received text from TT: {body}");
         serde_json::from_str::<O>(&body).map_err(|e| {
@@ -85,5 +114,17 @@ impl TTClient {
 
     pub async fn request_one<O: TTEndpoint>(&self, id: String) -> TTResult<O> {
         self.request_one_opt::<O, ()>(id, Option::<RequestOptions<()>>::None).await
+    }
+
+    pub async fn print_telemetry(&self) -> String {
+        let t = self.telemetry.lock().await;
+        format!(
+            "num requests: {}\nbytes received: {}\nelapsed time: {:.2}s\nrequests/s: {:.2}\nbytes/s: {}", 
+            t.requests,
+            t.bytes_received,
+            t.instant.elapsed().as_secs(),
+            t.requests as f64 / t.instant.elapsed().as_secs_f64(),
+            t.bytes_received as f64 / t.instant.elapsed().as_secs_f64()
+        )
     }
 }
